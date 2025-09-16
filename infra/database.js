c20aa6164fa9df74f879c1c0f823a5a9b25075fb
Paulo Montarroios <pauloricardomontarroios@hotmail.com>
2025-09-16 22:27:05 +0000

    Makes database.js more robust

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,21 @@ async function query(queryObject){
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD
     });
-    await client.connect();
+    console.log(`Credenciais do postgres:`,{
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        database: process.env.POSTGRES_DB,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD
+    } );
     
     try{
+        await client.connect();
         const result = await client.query(queryObject);
         return result;
-    }catch(e){
-        console.error(`Error: ${e}`)
+    }catch(error){
+        console.error(`Error: ${error}`)
+        throw error;
     }finally{
         await client.end();
     }
